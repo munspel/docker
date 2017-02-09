496e762456ece5c753177cdc0a499bd7d877b21b
unknown <D:\Documents\Munspel\the_bat>
2017-02-09 14:33:28 +0200

    'second'

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+/*require_once __DIR__.'/vendor/autoload.php';
 $config_path = __DIR__.'/src/config.php';
 
 use Docker\Core as Core;
 
 $app = Core::getInstance();
 $app->run($config_path);
+*/
+echo "Hello world!";
